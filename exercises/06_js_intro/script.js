84753788aee03c089d1cd5314dd59fee448eda98
Payne Whitlock <paynewhitlock@Paynes-MacBook-Pro-3.local>
2024-09-11 13:06:40 -0400

    upload

---
+++ exercises/06_js_intro/script.js
@@ -0,0 +1,16 @@
+const btnSayGoodbye = ()=>{
+    alert("hello world");
+}
+const myButton = document.getElementById("btn-click1");
+//myButton.onclick = doStuff;
+
+myButton.onclick = () => {
+    document.getElementById("message").innerHTML = "Hello everyone";
+    document.getElementById("Stuff").classList.add("special"); 
+};
+    document.getElementById("btn-click1").onlclick () => {
+        document.getElementById("message").innerHTML = "goodbye everyone";
+        document.getElementById("btn-click1").classList.remove("special");
+    };
+//Add a second button, called say goodbye
+//When clicked change the text to "goodbye everyone", and remove the special styles
