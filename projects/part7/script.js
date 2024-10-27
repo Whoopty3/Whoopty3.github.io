52cb7e372f8422b25889f9fd3b2184508c4146ea
Payne Whitlock <paynewhitlock@Paynes-MBP-3.attlocal.net>
2024-10-27 14:21:04 -0400

    upload

--- projects/part7/script.js
+++ projects/part7/script.js
@@ -120,3 +120,44 @@ document.getElementById("contactForm").addEventListener("submit", async function
         document.getElementById("errorMessage").style.display = "block";
     }
 });
+const sendEmail = async(json) => {
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: json
+        });
+
+        return response;
+    } catch (error) {
+        console.error(error);
+        document.getElementById("result").innerHTML = "Sorry, your email couldn't be sent";
+    }
+};
+document.getElementById("contact-form").onsubmit = async(e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+
+    
+    object.access_key = "10d455ab-caff-42c6-81d0-0af63b3109f5";  
+
+    const json = JSON.stringify(object);
+    
+    const result = document.getElementById("result");
+    result.innerHTML = "Sending...";
+
+    const httpResult = await sendEmail(json);
+
+    if (httpResult && httpResult.status == 200) {
+        result.innerHTML = "Email successfully sent";
+    } else {
+        result.innerHTML = "Sorry, your email wasn't sent";
+    }
+};
+
